c8fda1af2964e8f5553b1517bc37e781b3dd7ec1
Dmateosca <mateoscano.david@gmail.com>
2020-09-18 15:33:46 +0200

    ff

--- MEAN Stack/backend/calculadora.js
+++ MEAN Stack/backend/calculadora.js
@@ -6,7 +6,7 @@ var numero1 = parseFloat(params[0]);
 var numero2 = parseFloat(params[1]);
 
 var plantilla = `
-La suma es: ${numero1 + numero2}
+La suma es: ${numero1 + numero2} ;
 La resta es: ${numero1 - numero2}
 La multiplicación: ${numero1 * numero2}
 La división: ${numero1 / numero2}
