7514afc58477cf82a82c278a8d7a0cd9b740c129
Dmateosca <dmateosca@viewnext.com>
2020-09-18 15:03:45 +0200

    subida del backend a la altura del proyecto angular

---
+++ MEAN Stack/proyecto-angular/src/app/models/project.ts
@@ -0,0 +1,25 @@
+
+
+export class Project{
+    constructor(
+        public _id :string,
+        public name: string,
+        public description: string,
+        public category: string,
+        public year: number,
+        public langs: string,
+        public image: string
+    ){}
+
+}
+
+/*
+
+  "name": "David",
+  "description": "Mateos",
+  "category": "web",
+  "year": 2020,
+  "langs": "NODE",
+  "image": null,
+  
+  */
