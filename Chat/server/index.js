6bc0d4b2565a2483bcdd890ec3a24ee9406c17de
dmateosc <mateoscano.david@gmail.com>
2020-09-26 15:34:49 +0200

    Subida del index.js con la configuración inicial

--- Chat/server/index.js
+++ Chat/server/index.js
@@ -1,5 +1,17 @@
 'use strict'
-
+//configuración basica e importante
 var express = require('express');
-var socket = require('socket.io');
+var app = express();
+var server = require('http').Server(app);
 
+//Permitirá la escucha por puertos
+var socket = require('socket.io')(server);
+
+app.get('/hola-mundo',(req, res)=>{
+    
+    return res.status(200).send("Esto es una ruta");
+})
+
+server.listen(6677,()=>{
+    console.log("El servidor está escuchando");
+})
